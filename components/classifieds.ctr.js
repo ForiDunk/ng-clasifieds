3bfc52f34fa45e3fa40943de745d2ed8d0c4d85f
bela-dev <fori-prg@pathwwway.com>
2018-10-26 13:55:32 +0300

    10/26 13:55

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -4,10 +4,11 @@
 
     angular
         .module("ngClassifieds")
-        .controller("classifiedsCtrl", function($scope, $http, classifiedsFactory, $mdSidenav, $mdToast) {
+        .controller("classifiedsCtrl", function($scope, classifiedsFactory, $mdSidenav, $mdToast, $mdDialog) {
 
             classifiedsFactory.getClassifieds().then(function(classifieds) {
                 $scope.classifieds = classifieds.data;
+                $scope.categories = getCategories($scope.classifieds);
             });
 
             var contact = {
@@ -28,13 +29,51 @@
                     $scope.classifieds.push(classified);
                     $scope.classified = {};
                     $scope.closeSidebar();
-                    $mdToast.show(
-                        $mdToast.simple()
-                            .content("Classified Saved!")
-                            .position('top, right')
-                            .hideDelay(3000)
-                    );
+                    showToast("Classified Saved!");
                 }
+            };
+            $scope.editClassified = function(classified) {
+                $scope.editing = true;
+                $scope.openSidebar();
+                $scope.classified = classified;
+            };
+            $scope.saveEdit = function() {
+                $scope.editing = false;
+                $scope.classified = {};
+                $scope.closeSidebar();
+                showToast("Edit Saved!");
+            };
+            $scope.deleteClassfied = function(event, classified) {
+                var confirm = $mdDialog.confirm()
+                    .title("Are you sure you want to delete " + classified.title + '?')
+                    .ok('Yes')
+                    .cancel('No')
+                    .targetEvent(event);
+                $mdDialog.show(confirm).then(function() {
+                    var index = $scope.classifieds.indexOf(classified);
+                    $scope.classifieds.splice(index, 1);
+                }, function() {/* to do when the user clicks 'No' */});
+            };
+
+            function showToast(message) {
+                $mdToast.show(
+                    $mdToast.simple()
+                        .content(message)
+                        .position('top, right')
+                        .hideDelay(3000)
+                );
+            };
+
+            function getCategories(classifieds) {
+                var categories = [];
+
+                angular.forEach(classifieds, function(item) {
+                    angular.forEach(item.categories, function(category) {
+                        categories.push(category);
+                    });
+                });
+
+                return _.uniq(categories);
             };
         });
 })();
